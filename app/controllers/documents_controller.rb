a3e535eb648e1b6d4574e1d1a544bdcebe213317
FlevianK <flevian.kanaiza@andela.com>
2018-03-21 18:20:24 +0300

    [Feature #156163194] Add input validation function

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,11 +1,15 @@
 class DocumentsController < ApplicationController
   def new
+    @document = Document.new
   end
   def create
     @document = Document.new(document_params)
-    
-    @document.save
-    redirect_to @document
+
+    if @document.save
+      redirect_to @document
+    else
+      render 'new'
+    end
   end
   def show
     @document = Document.find(params[:id])
@@ -13,8 +17,8 @@ class DocumentsController < ApplicationController
   def index
     @documents = Document.all
   end
-  private
 
+  private
     def document_params
       params.require(:document).permit(:title, :text)
     end
