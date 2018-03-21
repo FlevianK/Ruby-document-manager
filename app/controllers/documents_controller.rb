d52c342edf2120cae96b6c70627e4d3330a84e12
FlevianK <flevian.kanaiza@andela.com>
2018-03-21 18:43:41 +0300

    [Feature #156165089] Add document delete resource

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -29,7 +29,12 @@ class DocumentsController < ApplicationController
       render 'edit'
     end
   end
-  
+  def destroy
+    @document = Document.find(params[:id])
+    @document.destroy
+ 
+    redirect_to documents_path
+  end
   private
   def document_params
       params.require(:document).permit(:title, :text)
