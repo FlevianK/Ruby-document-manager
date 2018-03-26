5d7933b425e878ae897d7233d1967786ef77f6b2
FlevianK <flevian.kanaiza@andela.com>
2018-03-26 16:42:19 +0300

    [Chore #156271039] Change database settings from sqlite3 to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180321195629) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "documents", force: :cascade do |t|
     t.string "title"
     t.text "text"
@@ -22,10 +25,11 @@ ActiveRecord::Schema.define(version: 20180321195629) do
   create_table "reviews", force: :cascade do |t|
     t.string "reviewer"
     t.text "body"
-    t.integer "document_id"
+    t.bigint "document_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["document_id"], name: "index_reviews_on_document_id"
   end
 
+  add_foreign_key "reviews", "documents"
 end
